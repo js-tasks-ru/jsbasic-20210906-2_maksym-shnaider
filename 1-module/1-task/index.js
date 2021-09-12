054fbe06e52be36ab95994970bbf9a7a31e3840e
unknown <maximshnaider@gmail.com>
2021-09-12 18:10:20 +0200

    2st task

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,11 @@
 function factorial(n) {
-  // ваш код...
+  for (let i = n-1; i=>0; i--){
+  n*=i;
+  }
+  return(n);
 }
+
+factorial(0); // 1
+factorial(1); // 1
+factorial(3); // 6
+factorial(5); // 120
