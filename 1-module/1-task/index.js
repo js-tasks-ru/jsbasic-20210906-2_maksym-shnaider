d6094816201961dd578521fbdba7666dd23eafbf
unknown <maximshnaider@gmail.com>
2021-09-13 21:41:33 +0200

    2st task

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,8 +1,12 @@
 function factorial(n) {
-  for (let i = n-1; i=>0; i--){
-  n*=i;
+  let result = 1;
+  if (n === 0 || n === 1) {
+    return result;
   }
-  return(n);
+
+  for (let i = 1; i <= n; i++) {
+    result = result * i;
+  }
+
+  return result;
 }
-
-
